0526c3ea901bfd3c71d7a94c92c22f219c778dbc
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-10-24 10:27:32 +0200

    [add] global error page

--- app/meals/error.js
+++ app/meals/error.js
@@ -1,3 +1,10 @@
+"use client";
+
 export default function Error() {
-  return;
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Faild to fetch meal data. Please try again later.</p>
+    </main>
+  );
 }
